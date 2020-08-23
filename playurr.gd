28e2adb364d3679286a0d094b68ebe69e006be7f
butterdome <49679950+butterdome@users.noreply.github.com>
2020-08-24 00:03:04 +0200

    New "Level1" Scene
    for testing how to implement art :v

---
+++ playurr.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+
+var motion = Vector2()
+var speed = 200
+
+func _physics_process(delta):
+	
+	if Input.is_action_pressed("ui_left"):
+		motion.x = -speed
+	elif Input.is_action_pressed("ui_right"):
+		motion.x = speed
+	else:
+		motion.x = 0
+	
+	
+	if Input.is_action_pressed("ui_up"):
+		motion.y = -speed
+	elif Input.is_action_pressed("ui_down"):
+		motion.y = speed
+	else:
+		motion.y = 0
+		
+		
+	move_and_slide(motion)
+	pass
